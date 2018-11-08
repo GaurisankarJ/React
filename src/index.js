ea49abf84a0156af88223ccfb6088e41301c93d2
Sankar <gaurisankarj@gmail.com>
2018-11-09 02:17:27 +0530

    Home Commit

--- src/index.js
+++ src/index.js
@@ -5,194 +5,62 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { connect } from "react-redux";
 import "./style.scss";
-import defaultVal from "./default";
 
 //Redux Store
-const ALTER = "ALTER";
-const JUST_EDITOR = "JUST_EDITOR";
-const JUST_PREVIEWER = "JUST_PREVIEWER";
-const BOTH = "BOTH";
+const POWER = "POWER";
+const BANK = "BANK";
+const INC_VOLUME = "INC_VOLUME";
+const DEC_VOLUME = "DEC_VOLUME";
 const defaultState = {
-    content: defaultVal,
-    editor: true,
-    previewer: true
+    power: false,
+    bank: false,
+    volume: 50
 };
-const markDownReducer = (state = defaultState, action) => {
+const drumReducer = (state = defaultState, action) => {
     switch(action.type) {
-        case ALTER: 
-            return {
-                content: action.content,
-                editor: state.editor,
-                previewer: state.previewer
-            };
-        case JUST_EDITOR:
-            return {
-                content: state.content,
-                editor: true,
-                previewer: false
-            };
-        case JUST_PREVIEWER:
-            return {
-                content: state.content,
-                editor: false,
-                previewer: true
-            };
-        case BOTH:
-            return {
-                content: state.content,
-                editor: true,
-                previewer: true
-            };
+        case POWER:
+            if(state.power == false) {
+                state.power = true;
+            } else {
+                state.power = false;
+            }
+            return state;
+        case BANK: 
+            if(state.bank == false) {
+                state.bank = true;
+            } else {
+                state.bank = true;
+            }
+            return state;
+        case INC_VOLUME:
+            state.volume ++;
+            return state;
+        case DEC_VOLUME: 
+            state.volume --;
+            return state;
         default:
             return state;
     }
 };
-const alterAction = (content) => {
+const powerAction = () => {
     return {
-        type: ALTER,
-        content: content
+        type: POWER
     };
 };
-const justEditorAction = () => {
+const bankAction = () => {
     return {
-        type: JUST_EDITOR
+        type: BANK
     };
 };
-const justPreviewerAction = () => {
+const incVolAction = () => {
     return {
-        type: JUST_PREVIEWER
+        type: INC_VOLUME
     };
 };
-const bothAction = () => {
+const decVolAction = () => {
     return {
-        type: BOTH
+        type: DEC_VOLUME
     };
 };
-const store = createStore(markDownReducer);
+const store = createStore(drumReducer);
 store.subscribe(() => console.log(store.getState()));
-
-//React Component
-class MarkdownPreviewer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClickEditor = this.handleClickEditor.bind(this);
-        this.handleClickPreviewer = this.handleClickPreviewer.bind(this);
-    }
-    handleChange(event) {
-        this.props.alterState(event.target.value);
-    }
-    getMarkDownText() {
-        var preMarked = this.props.stage.content.replace(/\\n/g, "<br />");
-        var markedDown = marked(preMarked);
-        return { __html: markedDown };
-    }
-    handleClickEditor() {
-        if(this.props.stage.previewer == false) {
-            this.props.both();
-            $("#editor").removeClass("maximized");
-        } else if(this.props.stage.previewer == true) {
-            this.props.justEditor();
-            $("#editor").addClass("maximized");
-        }
-        console.log("Clicked Editor!!");
-    }
-    handleClickPreviewer() {
-        if (this.props.stage.editor == false) {
-            this.props.both();
-            $("#previewer").removeClass("maximized");
-        } else if (this.props.stage.editor == true) {
-            this.props.justPreviewer();
-            $("#previewer").addClass("maximized");
-        }
-        console.log("Clicked Previewer!!");
-    }
-    render() {
-        var editorStyle = {
-            display: "block"
-        }; 
-        var previewerStyle = {
-            display: "block"
-        };
-        var clickImg = <i className="fa fa-arrows-alt"></i>;
-        if(this.props.stage.editor == true && this.props.stage.previewer == false) {
-            previewerStyle = {
-                display: "none"
-            }; 
-            clickImg = <i className="fa fa-compress" />;
-        } else if (this.props.stage.editor == false && this.props.stage.previewer == true) {
-            editorStyle = {
-                display: "none"
-            }; 
-            clickImg = <i className="fa fa-compress" />;
-        }
-        return (
-            <div>
-                <div id="editor" className="editor" style={editorStyle}>
-                    <header id="editor-header">
-                        <i className="fa fa-cannabis"></i>
-                        <span id="text">Editor</span>
-                        <span className="clicker" onClick={this.handleClickEditor}>
-                            {clickImg}
-                        </span>
-                    </header>
-                    <textarea id="text-area" className="work-area" value={this.props.stage.content} onChange={this.handleChange} />
-                </div>
-                <div id="previewer" className="previewer" style={previewerStyle}>
-                    <header id="editor-header">
-                        <i className="fa fa-cannabis"></i>
-                        <span id="text">Previewer</span>
-                        <span className="clicker" onClick={this.handleClickPreviewer}>
-                            {clickImg}
-                        </span>
-                    </header>
-                    <div id="view-area" className="work-area" dangerouslySetInnerHTML={this.getMarkDownText()} />
-                </div>
-            </div>
-        );
-    }
-}
-
-//Match state, dispatch to props
-const mapStateToProps = (state) => {
-    return {
-        stage: state
-    };
-};
-const mapDispatchToProps = (dispatch) => {
-    return {
-        alterState: (content) => {
-            dispatch(alterAction(content));
-        },
-        justEditor: () => {
-            dispatch(justEditorAction());
-        },
-        justPreviewer: () => {
-            dispatch(justPreviewerAction());
-        },
-        both: () => {
-            dispatch(bothAction());
-        }
-    };
-}; 
-
-//Connect Redux to React
-const Container = connect(mapStateToProps, mapDispatchToProps)(MarkdownPreviewer);
-
-class Presentation extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <Provider store={store}>
-                <Container />
-            </Provider>
-        );
-    }
-};
-
-ReactDOM.render(
-  <Presentation />,
-  document.getElementById("root")
-);
